ed0bf31616714714480b4c5954f7a67a19ee4208
AleSim94 <aleksandar.simeunovic94@gmail.com>
2017-03-27 09:42:31 +0200

    newsgroup

---
+++ clientserver/article.cc
@@ -0,0 +1,22 @@
+#include "article.h"
+#include <string>
+
+using namespace std;
+
+Article::Article(const string& ti, const string& au, const string& te, int& i) : title(ti), author(au), text(te), id(i) {}
+
+string Article::get_title() {
+  return title;
+}
+
+string Article::get_author() {
+  return author;
+}
+
+string Article::get_text() {
+  return text;
+}
+
+id Article::get_id() {
+  return id;
+}
