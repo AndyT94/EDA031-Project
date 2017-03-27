c3e1e81e57df449dc2c33afcb8ea206c2a74c31d
AndyT94 <andytruong94@gmail.com>
2017-03-27 23:07:56 +0200

    author,title,text

--- clientserver/database.h
+++ clientserver/database.h
@@ -7,7 +7,7 @@ public:
   virtual void create_newsgroup(const std::string& name) = 0;
   virtual void delete_newsgroup(int& group_id) = 0;
   virtual std::vector<Article> list_articles(int& group_id) const = 0;
-  virtual void create_article(int& group_id, Article& article) = 0;
+  virtual void create_article(int& group_id, std::string& title, std::string& author, std::string& text) = 0;
   virtual void delete_article(int& group_id, int& article_id) = 0;
   virtual Article get_article(int& group_id, int& article_id) const = 0;
 };
