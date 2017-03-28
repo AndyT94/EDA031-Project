cffb47e731609aa0da3a963b179112a39eb18fbf
AleSim94 <aleksandar.simeunovic94@gmail.com>
2017-03-28 13:13:31 +0200

    nu är den klar

--- clientserver/newsgroup.h
+++ clientserver/newsgroup.h
@@ -7,10 +7,10 @@ class NewsGroup {
 public:
   NewsGroup(int &id, string const name,std::std::map<int,Article> m);
   std::int get_newsgroupId();
-  std::vector<Article> getArticles(int &article_id);
+  std::vector<Article> get_articles();
   std::Article get_article(int &article_id const);
   std:: string get_name();
-  std::void delete_article();
+  std::void delete_article(int &article_id const);
   std::int get_size();
 
 private:
