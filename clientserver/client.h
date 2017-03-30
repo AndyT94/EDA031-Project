98af1d212c1361ee7ca4a9dfbd2ae657eec95b86
AleSim94 <aleksandar.simeunovic94@gmail.com>
2017-03-30 09:44:30 +0200

    client

---
+++ clientserver/client.h
@@ -0,0 +1,16 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include "connection.h"
+#include "server.h"
+#include <string>
+
+class Client{
+public:
+  void writeNumber(const Connection& conn, int value);
+
+  string readString(const Connection& conn);
+
+private:
+
+}
